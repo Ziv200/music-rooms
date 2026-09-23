d8a4d49e7d9eebb804092f8fa01cc4dd1ddea079
ziv200 <ziv200@gmail.com>
2026-09-23 14:22:00 +0300

    Updates

---
+++ _next/static/f4eyXAs19cn-AICy744A-/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/music-rooms//_next/:path+",
+        "destination": "/music-rooms/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
